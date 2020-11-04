b93f65c535d951392203f78f54e6edc7de53ca1c
Gleb <gleb.demin.04@mail.ru>
2020-11-04 17:59:16 +0500

    change db env

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class Config:
     # Database
     dirname = os.path.dirname(os.path.abspath(__file__))
     # print(f"sqlite:///{dirname}\\data.db")
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", f"sqlite:///{dirname}\\data.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", f"sqlite:///{dirname}\\data.db")
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
